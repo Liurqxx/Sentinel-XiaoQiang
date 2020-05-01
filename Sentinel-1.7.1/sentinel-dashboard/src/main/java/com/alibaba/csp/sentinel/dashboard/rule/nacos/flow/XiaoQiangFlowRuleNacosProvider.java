7a66c497ce0c717877b3504e80998611cc05d556
liurongqiang <liurongqiang@dhcc.com.cn>
2020-05-01 19:45:58 +0800

    添加流控规则提供者

--- Sentinel-1.7.1/sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/flow/XiaoQiangFlowRuleNacosProvider.java
+++ Sentinel-1.7.1/sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/flow/XiaoQiangFlowRuleNacosProvider.java
@@ -7,6 +7,7 @@ import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.csp.sentinel.util.StringUtil;
 import com.alibaba.nacos.api.config.ConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
  * @Description: 流控规则提供者
  * @Date: 2020/5/1 19:33
  */
+@Component("XiaoQiangFlowRuleNacosProvider")
 public class XiaoQiangFlowRuleNacosProvider implements DynamicRuleProvider<List<FlowRuleEntity>> {
 
     @Autowired
